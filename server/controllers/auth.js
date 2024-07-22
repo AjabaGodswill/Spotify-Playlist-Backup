4cb9e164bea3fdfc3ee0f417f543101f76209e14
AjabaGodwill <ajabagodswill@gmail.com>
2024-07-22 01:01:16 +0100

    Callback controller finished

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config()
-
+const axios = require('axios');
 // const { validationResult } = require('express-validator/check');
 
-const User = require('../models/User')
+const User = require('../models/User');
+const { error } = require('console');
 
 exports.login = (res, req, next) => {
     const scope = 'user-read-private user-read-email'
@@ -14,14 +15,48 @@ exports.login = (res, req, next) => {
         'response_type': 'code',
         'scope': scope,
         'redirect_uri': process.env.REDIRECT_URI,
-        'show_dialog': True
+        'show_dialog': true
     }
 
     const authUrl = `${process.env.Auth_URL}?${querystring.stringify(params)}`
 
-    // const urlParams = new
-
-    // const redirectUrl = `${auth_url}?${urlParams.toString()}/`
-
     return res.redirect(authUrl)
+
+}
+
+exports.callback = async (req, res, next) => {
+    if ('err' in req) {
+        return JSON.stringify({ "err": req['err'] })
+    }
+
+    if ('code' in req) {
+        req.body = {
+            'code': req.args['code'],
+            'grantType': 'authorizationCode',
+            'redirectUrl': process.env.REDIRECT_URI,
+            'client_id': process.env.SPOTIFY_CLIENT_ID,
+            'clientSecret': process.env.SPOTIFY_CLIENT_SECRET
+        }
+        try {
+
+
+            const response = await axios.post(process.env.TOKEN_URL, data = req.body)
+            tokenInfo = response.data
+
+            const expiresAt = Math.floor(Date.now() / 1000) + tokenInfo.expires_in;
+
+            req.session.accessToken = tokenInfo.accessToken
+            req.session.refreshToken = tokinInfo.refreshToken
+            req.session.expiresAt = expiresAt
+        } catch (error) {
+            console.error('Error fetching token:', error);
+            res.status(500).send('Error fetching token');
+        }
+
+        return res.redirect('/playlists')
+    }
+}
+
+exports.playlists = (req, res, next) => {
+
 }
--- server/index.js
+++ server/index.js
@@ -4,12 +4,21 @@ require('dotenv').config()
 const cors = require('cors')
 const connectDB = require('./config/database')
 const app = express()
+const session = require('express-session');
+
 
 
 const playlistRoutes = require('./routes/playlist');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.json()); // application/json
+
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using HTTPS
+  }));
 
 PORT = process.env.PORT || 5000
 
